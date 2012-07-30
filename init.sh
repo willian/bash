f90a60af65e1a17fcaa893c7b262210068f013e8
Willian Fernandes <willian@willianfernandes.com.br>
2012-07-30 11:24:41 -0300

    added Homebrew's bash completion

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+source `brew --prefix`/etc/bash_completion
 source $HOME/.bash/scripts/export.sh
 # source $HOME/.bash/scripts/rbenv.sh
 source $HOME/.bash/scripts/rvm.sh
@@ -5,7 +6,6 @@ source $HOME/.bash/scripts/alias.sh
 source $HOME/.bash/scripts/options.sh
 
 source $HOME/.bash/scripts/git_completion.sh
-source $HOME/.bash/scripts/bash_completion.sh
 source $HOME/.bash/scripts/gem_completion.sh
 source $HOME/.bash/scripts/utils.sh
 
